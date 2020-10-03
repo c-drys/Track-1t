3f52e23e83826657da30cfe6bdb27ef645e38ebd
drystar <chris.drysdale12@gmail.com>
2020-10-02 21:23:09 -0700

    add handle submit method | add methods to contructor

--- src/Components/CreateExercise.component.js
+++ src/Components/CreateExercise.component.js
@@ -4,6 +4,12 @@ export default class CreateExercise extends Component {
   constructor(props) {
     super(props);
 
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onChangeDescription = this.onChangeDescription.bind(this);
+    this.onChangeDuration = this.onChangeDuration.bind(this);
+    this.onChangeDate = this.onChangeDate.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
     // the properties of state will correspond to the fields in the MongoDB document
 
     this.state = {
@@ -34,6 +40,18 @@ export default class CreateExercise extends Component {
     this.setState({
       date: date,
     });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    const exercise = {
+      username: this.state.username,
+      description: this.state.description,
+      duration: this.state.duration,
+      date: this.state.date,
+    };
+    // console.log(exercise);
+    // window.location = "/";
   }
 
   render() {
