9717548c09aef54acec87f40de4bf5b5a1407b44
drystar <chris.drysdale12@gmail.com>
2020-10-02 21:18:00 -0700

    add methods to update state to CreateExercise

--- src/Components/CreateExercise.component.js
+++ src/Components/CreateExercise.component.js
@@ -14,6 +14,28 @@ export default class CreateExercise extends Component {
       users: [],
     };
   }
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value,
+    });
+  }
+  onChangeDescription(e) {
+    this.setState({
+      description: e.target.value,
+    });
+  }
+  onChangeDuration(e) {
+    this.setState({
+      duration: e.target.value,
+    });
+  }
+  onChangeDate(date) {
+    this.setState({
+      date: date,
+    });
+  }
+
   render() {
     return (
       <div>
