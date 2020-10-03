8152fcb0644343abbdba3f9f49960ed8587ceec1
drystar <chris.drysdale12@gmail.com>
2020-10-02 21:15:20 -0700

    add a constructor & state to CreateExercise

--- src/Components/CreateExercise.component.js
+++ src/Components/CreateExercise.component.js
@@ -1,6 +1,19 @@
 import React, { Component } from "react";
 
 export default class CreateExercise extends Component {
+  constructor(props) {
+    super(props);
+
+    // the properties of state will correspond to the fields in the MongoDB document
+
+    this.state = {
+      username: "",
+      description: "",
+      duration: 0,
+      date: new Date(),
+      users: [],
+    };
+  }
   render() {
     return (
       <div>
