a19f1ef9637b6c26742c19de18fe2f60c2d27dd8
drystar <chris.drysdale12@gmail.com>
2020-10-03 13:38:46 -0700

    add the delete method

--- src/Components/ExercisesList.component.js
+++ src/Components/ExercisesList.component.js
@@ -10,6 +10,16 @@ export default class ExercisesList extends Component {
     this.state = { exercises: [] };
   }
 
+  deleteExercise(id) {
+    axios
+      .delete("http://localhost:5000/exercises/" + id)
+      .then((res) => console.log(res.data));
+
+    this.setState({
+      exercises: this.state.exercises.filter((el) => el._id !== id),
+    });
+  }
+
   componentDidMount() {
     axios
       .get("http://localhost:5000/exercises/")
