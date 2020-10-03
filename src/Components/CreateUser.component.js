f52196be69cb10331133db3c2ab14a7dc5dc054f
drystar <chris.drysdale12@gmail.com>
2020-10-03 12:56:24 -0700

    create & return form

--- src/Components/CreateUser.component.js
+++ src/Components/CreateUser.component.js
@@ -33,7 +33,26 @@ export default class CreateUser extends Component {
   render() {
     return (
       <div>
-        <p>You are on the Create User component!</p>
+        <h3>Create New User</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Username: </label>
+            <input
+              type="text"
+              required
+              className="form-control"
+              value={this.state.username}
+              onChange={this.onChangeUsername}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="submit"
+              value="Create User"
+              className="btn btn-primary"
+            />
+          </div>
+        </form>
       </div>
     );
   }
