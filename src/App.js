07402dbd251d7897117e156c04dd2748d4cafece
drystar <chris.drysdale12@gmail.com>
2020-10-02 21:43:38 -0700

    tidy up index & App | update App favicon & name

--- src/App.js
+++ src/App.js
@@ -12,14 +12,12 @@ import CreateUser from "./Components/CreateUser.component";
 function App() {
   return (
     <Router>
-      <div className="container">
-        <Navbar />
-        <br />
-        <Route path="/" exact component={ExercisesList} />
-        <Route path="/edit/:id" component={EditExercise} />
-        <Route path="/create" component={CreateExercise} />
-        <Route path="/user" component={CreateUser} />
-      </div>
+      <Navbar />
+      <br />
+      <Route path="/" exact component={ExercisesList} />
+      <Route path="/edit/:id" component={EditExercise} />
+      <Route path="/create" component={CreateExercise} />
+      <Route path="/user" component={CreateUser} />
     </Router>
   );
 }
