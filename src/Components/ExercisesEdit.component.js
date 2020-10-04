ef6bdf9c3fb5c0648a46490487e4d176b100c002
drystar <chris.drysdale12@gmail.com>
2020-10-04 14:42:42 -0700

    update componentDidMount method | onSubmit method

--- src/Components/ExercisesEdit.component.js
+++ src/Components/ExercisesEdit.component.js
@@ -24,14 +24,23 @@ export default class EditExercise extends Component {
 
   componentDidMount() {
     axios
+      .get("http://localhost:5000/exercises/" + this.props.match.params.id)
+      .then((response) => {
+        this.setState({
+          username: response.data.username,
+          description: response.data.description,
+          duration: response.data.duration,
+          date: new Date(response.data.date),
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+    axios
       .get("http://localhost:5000/users/")
       .then((response) => {
-        if (response.data.length > 0) {
-          this.setState({
-            users: response.data.map((user) => user.username),
-            username: response.data[0].username,
-          });
-        }
+        this.setState({ users: response.data.map((user) => user.username) });
       })
       .catch((error) => {
         console.log(error);
@@ -64,13 +73,23 @@ export default class EditExercise extends Component {
 
   onSubmit(e) {
     e.preventDefault();
+
     const exercise = {
       username: this.state.username,
       description: this.state.description,
       duration: this.state.duration,
       date: this.state.date,
     };
+
     console.log(exercise);
+
+    axios
+      .post(
+        "http://localhost:5000/exercises/update/" + this.props.match.params.id,
+        exercise
+      )
+      .then((res) => console.log(res.data));
+
     window.location = "/";
   }
 
